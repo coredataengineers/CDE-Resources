b111c4378d76456c96db5a25bfd0770204957546
cappymayor <mayowaakinyele40@gmail.com>
2024-10-12 17:47:52 +0200

    s3 resource

--- terraform/iam_resource.tf
+++ terraform/iam_resource.tf
@@ -1,4 +1,4 @@
-# Create an iam user
+# Create an iam user resource
 
 resource "aws_iam_user" "lb" {
   name = "john_doe"
---
+++ terraform/s3_resource.tf
@@ -0,0 +1,5 @@
+# Create s3 bucket resource
+
+resource "aws_s3_bucket" "example" {
+  bucket = "cde-your-s3-bucket-name"
+}
